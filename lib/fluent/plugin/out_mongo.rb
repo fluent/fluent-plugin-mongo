7f5effd643cb4f045de36f6b646def5a0126055c
Masahiro Nakagawa <repeatedly@gmail.com>
2011-10-02 05:04:23 +0900

    Replace MongoBuffer with MongoOutput's <store>

--- lib/fluent/plugin/out_mongo.rb
+++ lib/fluent/plugin/out_mongo.rb
@@ -15,20 +15,31 @@ class MongoOutput < BufferedOutput
 
     raise ConfigError, "'database' parameter is required on Mongo output"   unless @database_name = conf['database']
     raise ConfigError, "'collection' parameter is required on Mongo output" unless @collection_name = conf['collection']
+    @host, @port = host_and_port(conf)
 
-    @host = conf['host'] || 'localhost'
-    @port = conf['port'] || 27017
-    @port = Integer(@port)
+    # capped configuration
+    if capped_conf = conf.elements.first
+      raise ConfigError, "'size' parameter is required on <store> of Mongo output" unless capped_conf.has_key?('size')
+      @capped_argument = {:capped => true}
+      @capped_argument[:size] = Integer(capped_conf['size'])
+      @capped_argument[:max]  = Integer(capped_conf['max']) if capped_conf.has_key?('max')
+
+      @capped_database_name = capped_conf['database'] || 'fluent'
+      @capped_collection_name = capped_conf['collection'] || '__backup'
+      @capped_host, @capped_port = host_and_port(capped_conf)
+    end
   end
 
   def start
     super
     @collection = Mongo::Connection.new(@host, @port).db(@database_name).collection(@collection_name)
+    @capped = capped_collection unless @capped_argument.nil?
   end
 
   def shutdown
     # Mongo::Connection checks alive or closed myself
     @collection.db.connection.close
+    @capped.db.connection.close unless @capped.nil?
     super
   end
 
@@ -45,7 +56,27 @@ class MongoOutput < BufferedOutput
         # EOFError always occured when reached end of chunk.
       end
     }
+
+    @capped.insert(records) unless @capped.nil?
     @collection.insert(records)
+  end
+
+  private
+
+  def host_and_port(conf)
+    host = conf['host'] || 'localhost'
+    port = conf['port'] || 27017
+    [host, Integer(port)]
+  end
+
+  def capped_collection
+    db = Mongo::Connection.new(@capped_host, @capped_port).db(@capped_database_name)
+    if db.collection_names.include?(@capped_collection_name)
+      # TODO: Verify capped configuraton
+      db.collection(@capped_collection_name)
+    else
+      db.create_collection(@capped_collection_name, @capped_argument)
+    end
   end
 end
 
