8a0f8553a305e5a9d6b421cb28179d74f7f7dd5f
Hiroshi Hatake <hatake@clear-code.com>
2015-07-02 11:56:10 +0900

    Add secret to parameters

--- lib/fluent/plugin/mongo_util.rb
+++ lib/fluent/plugin/mongo_util.rb
@@ -5,7 +5,7 @@ module MongoUtil
   def self.included(klass)
     klass.instance_eval {
       config_param :user, :string, :default => nil
-      config_param :password, :string, :default => nil
+      config_param :password, :string, :default => nil, :secret => true
     }
   end
 
